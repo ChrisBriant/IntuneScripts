eba480ffb348b765da0124c66851b70b7452905c
ChrisBriant <58183754+ChrisBriant@users.noreply.github.com>
2019-12-11 16:46:04 +0000

    Create ChangeLocalPassword.ps1

---
+++ ChangeLocalPassword.ps1
@@ -0,0 +1,3 @@
+﻿$localuser = Get-LocalUser -Name "TestLocalAdmin"
+$localpassword = ConvertTo-SecureString -String "NewLocalAdm1nPa$$w0rd!" -AsPlainText -Force
+$localuser | Set-LocalUser -Password $localpassword
